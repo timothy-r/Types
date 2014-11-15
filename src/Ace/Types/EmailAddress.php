3b1f5ad24050e68906b3c41f68f6d8aaa2198a0f
Tim Rodger <tim.rodger@gmail.com>
2014-11-15 15:31:26 +0000

    EmailAddress extends String

--- src/Ace/Types/EmailAddress.php
+++ src/Ace/Types/EmailAddress.php
@@ -1,45 +1,36 @@
 <?php namespace Ace\Types;
 
 use Ace\Types\TypeInterface;
+use Ace\Types\String;
 
 /**
 * Contains an email address
 */
-class EmailAddress implements TypeInterface
+class EmailAddress extends String
 {
-    /**
-    * @var string a valid email address or empty
-    */
-    private $value = '';
-   
-    private $pattern = '/^.+@.+$/';
-
-    public function __construct($value)
-    {
-        if (is_string($value) && preg_match($this->pattern, $value)){
-            $this->value = $value;
-        }
-    }
-
     /**
     * @return string
     */
     public function __toString()
     {
-        return $this->value;
+        return $this->value();
     }
     
     /**
-    * @return array
+    * @return string
     */
     public function value()
     {
-        return $this->value;
+        return $this->valid() ? parent::value() : '';
     }
 
+    /**
+    * @return boolean
+    */
     public function valid()
     {
-        return !empty($this->value);
+        $pattern = '/^.+@.+$/';
+        return (boolean) preg_match($pattern, parent::value());
     }
 }
 
